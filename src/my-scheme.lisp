d0bace12ca1efcd61e0bad8eaddb12b59201dc0f
wasuken <wevorence@gmail.com>
2019-08-03 15:46:28 +0900

    integration variable and method alist.
    next, rewrite parser.

--- src/my-scheme.lisp
+++ src/my-scheme.lisp
@@ -13,26 +13,19 @@
 	  (push arg run-lst))
 	(eval (reverse run-lst))))
 
-(defparameter *methods-alist*
+(defparameter *variable-alist*
   `(("+" ,(function (lambda (x y) (+ x y))))
 	("-" ,(function (lambda (x y) (- x y))))
 	("*" ,(function (lambda (x y) (* x y))))
-	("/" ,(function (lambda (x y) (/ x y))))))
-
-(defun getmethod (method)
-  (cadr (find-if #'(lambda (x) (string= (car x) method)) *methods-alist*)))
-
-(defun setmethod (method lambda)
-  (push '(method . lambda) *methods-alist*))
-
-(defparameter *variable-number-alist*
-  '())
+	("/" ,(function (lambda (x y) (/ x y))))
+	("def" ,(function (lambda (x y) (push `(,x ,y) *variable-alist*))))
+	("lambda"  ,(function (lambda (x y)　(lambda x y))))))
 
 (defun getvar (var)
-  (cadr (find-if #'(lambda (x) (string= (car x) var)) *variable-number-alist*)))
+  (cadr (find-if #'(lambda (x) (string= (car x) var)) *variable-alist*)))
 
 (defun setvar (var contents)
-  (push '(var . contents) *variable-number-alist*))
+  (push `(,var ,contents) *variable-alist*))
 
 (defmacro case-str (x &body body)
   (let ((cond-list `(cond)))
@@ -78,43 +71,35 @@
 												(closure-html:make-lhtml-builder))))
 										 " ")))
 	(tree-remove-if to-html-lst :a)))
-
+;;; 未実装
 (defun intepretor ())
 
-;; (defun parser-method (lst)
-;;   (mapcar #'(lambda (x)
-;; 			  (cond ((listp x)
-;; 					 (parser-method x))
-;; 					((ppcre:scan-to-strings "\\(|\\)" x)
-;; 					 x)
-;; 					((ppcre:scan-to-strings "^[0-9]+" x)
-;; 					 (parse-integer x))
-;; 					((getmethod x)
-;; 					 (getmethod x))
-;; 					(t (getvar x))))
-;; 		  lst))
-
-;; (defun parser (str)
-;;   (str-parse str))
-
 (defun lexer-method (lst)
-  (let* ((formula-str (ppcre:regex-replace-all "\\s+" (string-trim '(#\Space) (format nil "~{~A ~}"
-													  (remove-if #'(lambda (x) (= (length x) 0))
-																 (mapcar #'(lambda (x) (cond ((null x) "")
-																							 ((listp x)
-																							  (lexer-method x))
-																							 ((and (stringp x)
-																								  (string= (string-trim '(#\Space) x) ""))
-																							  "")
-																							 (t x)))
-																		 lst))))
+  (let* ((formula-lst (remove-if #'(lambda (x) (= (length x) 0))
+						 (mapcar #'(lambda (x) (cond ((null x) "")
+													 ((listp x)
+													  (lexer-method x))
+													 ((and (stringp x)
+														   (string= (string-trim '(#\Space) x) ""))
+													  "")
+													 (t x)))
+								 lst)))
+		 (formula-str (ppcre:regex-replace-all "\\s+"
+											   (string-trim '(#\Space) (format nil "~{~A ~}"
+																			   formula-lst))
 											   " "))
 		 (method (ppcre:scan-to-strings "^\\S+" formula-str))
 		 ;; 今は数字のみ扱う
-		 (args (mapcar #'(lambda (x) (if (not (string= "" x)) (parse-integer x))) (cdr (ppcre:split " " formula-str)))))
-	(write-to-string (funcall-args-cons (getmethod method) args))))
-
-
+		 (args (mapcar #'(lambda (x)
+						   (cond ((ppcre:scan-to-strings "^([a-z|A-Z][0-9|\\w]+|[a-z|A-Z])" x)
+								  (if (string= method "def")
+									  x
+									  (getvar x)))
+								 ((ppcre:scan-to-strings "^[0-9]+")
+								  (parse-integer x))))
+					   (cdr (ppcre:split " " formula-str)))))
+	(print method)
+	(write-to-string (funcall-args-cons (getvar method) args))))
 
 (defun lexer (str)
   (lexer-method (parser str)))
--- t/my-scheme-test.lisp
+++ t/my-scheme-test.lisp
@@ -3,23 +3,31 @@
   (:use :cl :prove :my-scheme))
 (in-package #:my-scheme-test)
 
+;;; やりたいことをテストとして書いていきたい。
+
 (plan 2)
 
 (subtest "Testing parser"
-		 (let ((result (my-scheme:parser "(+ 1 2)")))
-		   (diag "first function")
-		   (ok result "+ 1 2")
-		   )
-		 (let ((result (my-scheme:parser "(+ 1 (* 2 3)")))
-		   (diag "first function")
-		   (is result '("+ 1 " ("* 2 3")))
-		   )
-		 )
+  (let ((result (my-scheme:parser "(+ 1 2)")))
+	(diag "first function")
+	(ok result "+ 1 2")
+	)
+  (let ((result (my-scheme:parser "(+ 1 (* 2 3)")))
+	(diag "first function")
+	(is result '("+ 1 " ("* 2 3")))
+	)
+  )
 
 (subtest "Testing lexer"
-		 (is (my-scheme:lexer "(+ 1 2)")
-			 "3")
-		 (is (my-scheme:lexer "(+ 1 (* 2 3)")
-			 "7")
-		 (is (my-scheme:lexer "(+ (* 50 2) (* 25 4))")
-			 "200"))
+  (is (my-scheme:lexer "(+ 1 2)")
+	  "3")
+  (is (my-scheme:lexer "(+ 1 (* 2 3)")
+	  "7")
+  (is (my-scheme:lexer "(+ (* 50 2) (* 25 4))")
+	  "200"))
+
+(subtest "Variable Test"
+  (lexer "(def c 10)")
+  (is (lexer "(+ 1 c)") "11")
+  (lexer "(def d 11)")
+  (is (lexer "(+ c d)") "21"))
